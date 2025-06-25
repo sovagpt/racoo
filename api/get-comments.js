40af9c87c7f868cb0f948730a1b18fc3cbb73c34
Sova <khielsgang@gmail.com>
2025-06-25 02:25:40 +0100

    Update get-comments.js

--- api/get-comments.js
+++ api/get-comments.js
@@ -5,32 +5,30 @@ const client = createClient({
 });
 
 export default async function handler(req, res) {
-  // Add CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   try {
-    console.log('Getting comments...');
-    
-    // Test KV connection
-    const testKey = await kv.get('test');
-    console.log('KV test result:', testKey);
+    await client.connect();
     
     // Get last 20 comments
-    const comments = await kv.lrange('comments', 0, 19);
-    console.log('Comments from KV:', comments);
+    const comments = await client.lRange('comments', 0, 19);
     
     const parsedComments = comments.map(comment => JSON.parse(comment));
 
+    await client.disconnect();
+    
     res.json(parsedComments);
 
   } catch (error) {
     console.error('Get comments error:', error);
+    try {
+      await client.disconnect();
+    } catch (e) {}
     res.status(500).json({ 
       error: 'Failed to get comments',
-      details: error.message,
-      stack: error.stack 
+      details: error.message
     });
   }
 }
